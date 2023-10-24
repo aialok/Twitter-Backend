8cd60674c1ed3b83bef2a33ae572dc654ca39cc1
aialok <alokgupta1560@gmail.com>
2023-10-24 11:17:09 +0530

    Added Basic Universal  Crud-Repository

---
+++ src/repository/crud-repository.js
@@ -0,0 +1,67 @@
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    try {
+
+        const result = await this.model.create(data);
+        return result;
+
+    } catch (error) {
+      console.log("Something went wrong in the crud Repository");
+      throw error;
+    }
+  }
+
+  async destroy(id) {
+    try {
+
+        const result = await this.model.findByIdAndDelete(id);
+        return result;
+
+    } catch (error) {
+      console.log("Something went wrong in the crud Repository");
+      throw error;
+    }
+  }
+
+  async get(id) {
+    try {
+
+        const result = await this.model.findById(id);
+        return result;
+
+    } catch (error) {
+      console.log("Something went wrong in the crud Repository");
+      throw error;
+    }
+  }
+
+  async getAll(){
+      try {
+          const result = await this.model.find({});
+          return result;
+      } catch (error) {
+        console.log("Something went wrong in the crud Repository");
+        throw error;
+      }
+  }
+
+  async update(data) {
+    try {
+
+        const result = await this.model.findByIdAndUpdate(id,data, {new : true})
+        return result;
+
+    } catch (error) {
+      console.log("Something went wrong in the crud Repository");
+      throw error;
+    }
+  }
+
+
+}
+
+export default CrudRepository;
